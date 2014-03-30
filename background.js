7a01e2c9183ceda76a4a328c3dbb77d1fa27963a
IzumiSy <beetle-noise@gmx.com>
2014-03-30 22:27:43 +0900

    Added a function to remove current tab's url from recorded histories

--- background.js
+++ background.js
@@ -7,12 +7,25 @@ var isExtensionWorking;
 chrome.browserAction.setBadgeBackgroundColor({color: "#00FF00"});
 isExtensionWorking = false;
 
+function deleteCurrentTab()
+{
+	chrome.tabs.getSelected(window.id, function(tab) {
+		chrome.history.search({text: tab.url}, function(results) {
+			results.forEach(function(rs, i) {
+				console.log("Deleted: " + rs.url);
+				// chrome.history.deleteUrl({url: rs.url});
+			});
+		});
+	});
+}
+
 chrome.browserAction.onClicked.addListener(function() {
 	var status;
 	
 	isExtensionWorking = ! isExtensionWorking;
 	if (isExtensionWorking == true) {
 		status = "ON";
+		deleteCurrentTab();
 	} else {
 		status = "";
 	}
