2aaf3d211ba442dec0e2846f1d102ebe175581a9
IzumiSy <beetle-noise@gmx.com>
2014-07-19 15:13:27 +0900

    Added basement codes for advanced option.

--- options.js
+++ options.js
@@ -1,2 +1,12 @@
 
 // options.js
+
+document.getElementById("remove").onclick = function() {
+	var options = document.getElementById("period").options;
+
+	for (var i = 0;i < options.length;i++) {
+		if (options[i].selected) {
+			console.log(options[i].value);
+		}
+	}
+}
