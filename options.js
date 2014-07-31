9a44a301aaf67cd7e7ae668b5b9ff3e980ef1425
IzumiSy <beetle-noise@gmx.com>
2014-07-31 15:12:28 +0900

    Added codes for advanced remove tool

--- options.js
+++ options.js
@@ -3,10 +3,25 @@
 
 document.getElementById("remove").onclick = function() {
 	var options = document.getElementById("period").options;
+	var onehour = 3600000;
+	var during
 
 	for (var i = 0;i < options.length;i++) {
 		if (options[i].selected) {
-			console.log(options[i].value);
+			switch (options[i].value) {
+				case "1hour": during = onehour; break;
+				case "6hours": during = onehour * 6; break;
+				case "12hours": during = onehour * 12; break;
+				case "1day": during = onehour * 24; break;
+			}
+			chrome.browsingData.removeHistory(
+				{
+					"since": (new Date()).getTime() - during,
+					"originTypes": {
+						"unprotectedWeb": true
+					}
+				}
+			);
 		}
 	}
 }
