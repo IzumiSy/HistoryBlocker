4e01170fd483724ef720ea5cc84fbcb73db564e8
IzumiSy <beetle-noise@gmx.com>
2014-08-05 13:40:52 +0900

    Added some base codes to record browsing activities during working

--- background.js
+++ background.js
@@ -10,6 +10,8 @@ const OPTION_REMOVE_CACHE = "RemoveCache";
 // Initialize
 isExtensionWorking = false;
 workingSince = 0;
+dataHistory = [];
+temporalHistory = [];
 
 chrome.tabs.onUpdated.addListener(function(id, info, tab) {
 	if (isExtensionWorking) {
@@ -26,6 +28,10 @@ chrome.tabs.onUpdated.addListener(function(id, info, tab) {
 					"cache": localStorage.getItem(OPTION_REMOVE_CACHE) == "true" ? true : false
 				}, function() {
 					console.log("Removed: " + tab.url);
+					temporalHistory.push({
+						"title": tab.title,
+						"url": tab.url
+					});
 				}
 			);
 		}
@@ -40,9 +46,11 @@ function toggleActivation()
 	if (isExtensionWorking == true) {
 		status = "ON";
 		workingSince = (new Date()).getTime();
+		temporalHistory = [];
 		console.log("Activated on: " + workingSince);
 	} else {
 		status = "";
+		dataHistory.push(temporalHistory);
 	}
 	chrome.browserAction.setBadgeText({text: status});
 }
