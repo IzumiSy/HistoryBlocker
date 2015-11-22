8017093f6c133150f3db12c0f5e3b74bd71d2def
IzumiSy <beetle-noise@gmx.com>
2015-11-22 11:40:39 +0900

    Fix indentations

--- js/background.js
+++ js/background.js
@@ -16,49 +16,49 @@ workingSince = 0;
 workingHistory = [];
 
 chrome.tabs.onUpdated.addListener(function(id, info, tab) {
-	if (isExtensionWorking) {
-		if (info.status == "complete") {
-			console.log("Removed: " + tab.url);
-			workingHistory.unshift({"title": tab.title, "url": tab.url});
-		}
-	}
+  if (isExtensionWorking) {
+    if (info.status == "complete") {
+      console.log("Removed: " + tab.url);
+      workingHistory.unshift({"title": tab.title, "url": tab.url});
+    }
+  }
 });
 
 function toggleActivation()
 {
-	var md;
-	var status;
-	
-	isExtensionWorking = ! isExtensionWorking;
-	status = isExtensionWorking === true ? "ON" : "";
-	md = new Date();
-	if (isExtensionWorking === true) {
-		workingSince = md.getTime();
-		workingHistory.unshift({"title": LOG_ACTIVATED, "url": md.toString()});
-	} else {
-		chrome.browsingData.remove(
-			{
-				"since": workingSince,
-				"originTypes": {
-					"unprotectedWeb": true
-				}
-			}, {
-				"history": true,
-				"cookies": localStorage.getItem(OPTION_REMOVE_COOKIES) == "true" ? true : false,
-				"cache": localStorage.getItem(OPTION_REMOVE_CACHE) == "true" ? true : false
-			}
-		);
-		workingHistory.unshift({"title": LOG_DEACTIVATED, "url": md.toString()});
-		localStorage.setItem(WORKING_HISTORY, JSON.stringify(workingHistory));
-		temporalHistory = [];
-	}
-	chrome.browserAction.setBadgeText({text: status});
+  var md;
+  var status;
+
+  isExtensionWorking = ! isExtensionWorking;
+  status = isExtensionWorking === true ? "ON" : "";
+  md = new Date();
+  if (isExtensionWorking === true) {
+    workingSince = md.getTime();
+    workingHistory.unshift({"title": LOG_ACTIVATED, "url": md.toString()});
+  } else {
+    chrome.browsingData.remove(
+      {
+        "since": workingSince,
+        "originTypes": {
+          "unprotectedWeb": true
+        }
+      }, {
+        "history": true,
+        "cookies": localStorage.getItem(OPTION_REMOVE_COOKIES) == "true" ? true : false,
+        "cache": localStorage.getItem(OPTION_REMOVE_CACHE) == "true" ? true : false
+      }
+    );
+    workingHistory.unshift({"title": LOG_DEACTIVATED, "url": md.toString()});
+    localStorage.setItem(WORKING_HISTORY, JSON.stringify(workingHistory));
+    temporalHistory = [];
+  }
+  chrome.browserAction.setBadgeText({text: status});
 }
 
 chrome.commands.onCommand.addListener(function(command) {
-	if (command == "toggle-blocking") {
-		toggleActivation();
-	}
+  if (command == "toggle-blocking") {
+    toggleActivation();
+  }
 });
 
 chrome.browserAction.onClicked.addListener(toggleActivation);
--- js/options.js
+++ js/options.js
@@ -2,10 +2,10 @@
 // options.js
 
 (function(){
-  const FAVICON_API = "http://www.google.com/s2/favicons?domain=";  
-  
+  const FAVICON_API = "http://www.google.com/s2/favicons?domain=";
+
   var BG = chrome.extension.getBackgroundPage();
-  
+
   var elements = {
     captions: {
       pageTitle:      document.getElementById("page-title"),
@@ -17,180 +17,180 @@
       warningMessage: document.getElementById("warning-message"),
       historyCaption: document.getElementById("history-caption")
     },
-    
+
     inputs: {
       removeCookies: document.getElementById("remove-cookies"),
       removeCache:   document.getElementById("remove-cache"),
       selectPeriod:  document.getElementById("period")
-    },    
-    
+    },
+
     buttons: {
       remove: document.getElementById("remove-button"),
       clear:  document.getElementById("clear-button"),
       save:   document.getElementById("save-button"),
       cancel: document.getElementById("cancel-button")
     },
-    
+
     tabs: {
       options: document.getElementById("options"),
       history: document.getElementById("history")
     }
   };
-  
+
   var alertTxts = {
     cleanUp: chrome.i18n.getMessage("extCleanUpAlert"),
     historyRemove: chrome.i18n.getMessage("extHistoryRemoveAlert")
   };
-  
+
   var miscFunctions = {
     changeTab: function(tab) {
-    	elements.tabs.options.style.display = "none";
-    	elements.tabs.history.style.display = "none";
-    	document.getElementById(tab).style.display = "block";
+      elements.tabs.options.style.display = "none";
+      elements.tabs.history.style.display = "none";
+      document.getElementById(tab).style.display = "block";
     },
-  
+
     setupHandlers: function() {
       elements.buttons.remove.onclick = onClickHandlers.removeButton;
       elements.buttons.save.onclick   = onClickHandlers.saveButton;
       elements.buttons.cancel.onclick = onClickHandlers.cancelButton;
       elements.buttons.clear.onclick  = onClickHandlers.clearHistoryButton;
       elements.captions.tabOptions.onclick = onClickHandlers.tabs.options;
-      elements.captions.tabHistory.onclick = onClickHandlers.tabs.history;      
+      elements.captions.tabHistory.onclick = onClickHandlers.tabs.history;
     },
-  
+
     loadCustomHistory: function() {
-    	var newAnchor, newList, newDiv, newFavicon;
-    	var mainMenu = document.getElementById("items");
-    	var items = [];
-    
-    	items = JSON.parse(localStorage.getItem(BG.WORKING_HISTORY));
-    	if (items) {
-    		items.forEach(function(item, i) {
-    			newAnchor = document.createElement("a");
-    			newList = document.createElement("li");
-    			newSpan = document.createElement("span");
-    			if (item["title"] == BG.LOG_ACTIVATED || item["title"] == BG.LOG_DEACTIVATED) {
-    				newAnchor.innerHTML = "<b>" + item["title"] + item["url"] + "</b>";
-    			} else {
-    				newFavicon = document.createElement("img");
-    				newFavicon.setAttribute("src", FAVICON_API + item["url"]);
-    				newFavicon.setAttribute("class", "favicon");
-    				newAnchor.setAttribute("href", item["url"]);
-    				newAnchor.appendChild(newFavicon);
-    				newAnchor.appendChild(document.createTextNode(item["title"]));
-    				newSpan.appendChild(document.createTextNode(" " + item["url"]));
-    				newSpan.setAttribute("style", "color: #C0C0C0");
-    			}
-    			newList.appendChild(newAnchor);
-    			newList.appendChild(newSpan);
-    			mainMenu.appendChild(newList);
-    		});
-    	}
+      var newAnchor, newList, newDiv, newFavicon;
+      var mainMenu = document.getElementById("items");
+      var items = [];
+
+      items = JSON.parse(localStorage.getItem(BG.WORKING_HISTORY));
+      if (items) {
+        items.forEach(function(item, i) {
+          newAnchor = document.createElement("a");
+          newList = document.createElement("li");
+          newSpan = document.createElement("span");
+          if (item["title"] == BG.LOG_ACTIVATED || item["title"] == BG.LOG_DEACTIVATED) {
+            newAnchor.innerHTML = "<b>" + item["title"] + item["url"] + "</b>";
+          } else {
+            newFavicon = document.createElement("img");
+            newFavicon.setAttribute("src", FAVICON_API + item["url"]);
+            newFavicon.setAttribute("class", "favicon");
+            newAnchor.setAttribute("href", item["url"]);
+            newAnchor.appendChild(newFavicon);
+            newAnchor.appendChild(document.createTextNode(item["title"]));
+            newSpan.appendChild(document.createTextNode(" " + item["url"]));
+            newSpan.setAttribute("style", "color: #C0C0C0");
+          }
+          newList.appendChild(newAnchor);
+          newList.appendChild(newSpan);
+          mainMenu.appendChild(newList);
+        });
+      }
     },
-    
+
     loadPreferences: function() {
-  	  elements.inputs.removeCookies.checked = localStorage.getItem(BG.OPTION_REMOVE_COOKIES) == "true" ? true : false;
-  	  elements.inputs.removeCache.checked   = localStorage.getItem(BG.OPTION_REMOVE_CACHE) == "true" ? true : false;      
+      elements.inputs.removeCookies.checked = localStorage.getItem(BG.OPTION_REMOVE_COOKIES) == "true" ? true : false;
+      elements.inputs.removeCache.checked   = localStorage.getItem(BG.OPTION_REMOVE_CACHE) == "true" ? true : false;
     },
-    
+
     i18nApply: function() {
-    	elements.captions.pageTitle.textContent      = chrome.i18n.getMessage("extOptionTitle");
-    	elements.captions.tabOptions.textContent     = chrome.i18n.getMessage("extTabOptions");
-    	elements.captions.tabHistory.textContent     = chrome.i18n.getMessage("extTabHistory");
-    	elements.captions.removePeriod.textContent   = chrome.i18n.getMessage("extRemovePeriod");
-    	elements.captions.warningMessage.innerHTML   = chrome.i18n.getMessage("extWarningMessage");
-    	elements.captions.historyCaption.textContent = chrome.i18n.getMessage("extCustomHistory");
-    	
-    	elements.captions.removeCookies.textContent = chrome.i18n.getMessage("extRemoveCookies");
-    	elements.captions.removeCache.textContent   = chrome.i18n.getMessage("extRemoveCache");  	
-    	elements.buttons.remove.textContent = chrome.i18n.getMessage("extRemoveButton");
-    	elements.buttons.save.textContent   = chrome.i18n.getMessage("extSaveButton");
-    	elements.buttons.cancel.textContent = chrome.i18n.getMessage("extCancelButton");
-      elements.buttons.clear.textContent  = chrome.i18n.getMessage("extClearAllButton");      
+      elements.captions.pageTitle.textContent      = chrome.i18n.getMessage("extOptionTitle");
+      elements.captions.tabOptions.textContent     = chrome.i18n.getMessage("extTabOptions");
+      elements.captions.tabHistory.textContent     = chrome.i18n.getMessage("extTabHistory");
+      elements.captions.removePeriod.textContent   = chrome.i18n.getMessage("extRemovePeriod");
+      elements.captions.warningMessage.innerHTML   = chrome.i18n.getMessage("extWarningMessage");
+      elements.captions.historyCaption.textContent = chrome.i18n.getMessage("extCustomHistory");
+
+      elements.captions.removeCookies.textContent = chrome.i18n.getMessage("extRemoveCookies");
+      elements.captions.removeCache.textContent   = chrome.i18n.getMessage("extRemoveCache");
+      elements.buttons.remove.textContent = chrome.i18n.getMessage("extRemoveButton");
+      elements.buttons.save.textContent   = chrome.i18n.getMessage("extSaveButton");
+      elements.buttons.cancel.textContent = chrome.i18n.getMessage("extCancelButton");
+      elements.buttons.clear.textContent  = chrome.i18n.getMessage("extClearAllButton");
     }
   };
-  
+
   var onClickHandlers = {
     tabs: {
-      options: function() { 
+      options: function() {
         miscFunctions.changeTab("options");
         return false;
       },
-      
-      history: function() { 
+
+      history: function() {
         miscFunctions.changeTab("history");
-        return false; 
+        return false;
       }
     },
-    
+
     clearHistoryButton: function() {
-    	var items = document.getElementById("items");
-    
-    	while (items.firstChild) {
-    		items.removeChild(items.firstChild);
-    	}
-    	localStorage.removeItem(BG.WORKING_HISTORY);
-    	alert(alertTxts.cleanUp);
+      var items = document.getElementById("items");
+
+      while (items.firstChild) {
+        items.removeChild(items.firstChild);
+      }
+      localStorage.removeItem(BG.WORKING_HISTORY);
+      alert(alertTxts.cleanUp);
     },
-    
+
     removeButton: function() {
-    	elements.buttons.remove.disabled = true;
-    	elements.inputs.selectPeriod.disabled = true;
-    	
-    	var options = elements.inputs.selectPeriod.options; 	
-    	var onehour = 3600000;
-    	var args, during;
-    
-    	for (var i = 0;i < options.length;i++) {
-    		if (options[i].selected) {
-    			switch (options[i].value) {
-    				case "1hour": during = onehour; break;
-    				case "6hours": during = onehour * 6; break;
-    				case "12hours": during = onehour * 12; break;
-    				case "1day": during = onehour * 24; break;
-    			}
-    			break;
-    		}
-    	}
-    	
+      elements.buttons.remove.disabled = true;
+      elements.inputs.selectPeriod.disabled = true;
+
+      var options = elements.inputs.selectPeriod.options;
+      var onehour = 3600000;
+      var args, during;
+
+      for (var i = 0;i < options.length;i++) {
+        if (options[i].selected) {
+          switch (options[i].value) {
+            case "1hour": during = onehour; break;
+            case "6hours": during = onehour * 6; break;
+            case "12hours": during = onehour * 12; break;
+            case "1day": during = onehour * 24; break;
+          }
+          break;
+        }
+      }
+
       args = {
         misc: {
-				  "since": (new Date()).getTime() - during,
-				  "originTypes": { "unprotectedWeb": true }
+          "since": (new Date()).getTime() - during,
+          "originTypes": { "unprotectedWeb": true }
         },
-        
+
         targets: {
-				  "history": true,
-				  "cookies": localStorage.getItem(BG.OPTION_REMOVE_COOKIES) == "true" ? true : false,
-				  "cache": localStorage.getItem(BG.OPTION_REMOVE_CACHE) == "true" ? true : false
+          "history": true,
+          "cookies": localStorage.getItem(BG.OPTION_REMOVE_COOKIES) == "true" ? true : false,
+          "cache": localStorage.getItem(BG.OPTION_REMOVE_CACHE) == "true" ? true : false
         }
-			};			
-    	
-      chrome.browsingData.remove(args.misc, args.targets, onClickHandlers._handler_browsingDataRemove);    	
+      };
+
+      chrome.browsingData.remove(args.misc, args.targets, onClickHandlers._handler_browsingDataRemove);
     },
-    
+
     _handler_browsingDataRemove: function() {
-			elements.buttons.remove.disabled = false;
-			elements.inputs.selectPeriod.disabled = false;
-			alert(alertTxts.historyRemove);
-		},
-    
+      elements.buttons.remove.disabled = false;
+      elements.inputs.selectPeriod.disabled = false;
+      alert(alertTxts.historyRemove);
+    },
+
     saveButton: function() {
-    	localStorage.setItem(BG.OPTION_REMOVE_COOKIES, elements.inputs.removeCookies.checked);
-    	localStorage.setItem(BG.OPTION_REMOVE_CACHE, elements.inputs.removeCache.checked);
-    	window.close();
-    }, 
-    
+      localStorage.setItem(BG.OPTION_REMOVE_COOKIES, elements.inputs.removeCookies.checked);
+      localStorage.setItem(BG.OPTION_REMOVE_CACHE, elements.inputs.removeCache.checked);
+      window.close();
+    },
+
     cancelButton: function() {
-    	window.close();
+      window.close();
     }
   };
-  
+
   document.body.onload = function() {
     miscFunctions.loadPreferences();
     miscFunctions.loadCustomHistory();
     miscFunctions.setupHandlers();
-    miscFunctions.i18nApply();    
-    miscFunctions.changeTab("options");	
-  };  
+    miscFunctions.i18nApply();
+    miscFunctions.changeTab("options");
+  };
 })();
